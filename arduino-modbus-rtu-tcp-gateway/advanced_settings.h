539dc8c7cfd0b54ce6a89958088676520c3bc98d
Samuel Cabrero <samuel@orica.es>
2025-02-12 22:27:51 +0100

    Fix compiler warning
    arduino-modbus-rtu-tcp-gateway/advanced_settings.h:69:50: warning: multi-line comment [-Wcomment]
    
    Signed-off-by: Samuel Cabrero <samuel@orica.es>

--- arduino-modbus-rtu-tcp-gateway/advanced_settings.h
+++ arduino-modbus-rtu-tcp-gateway/advanced_settings.h
@@ -66,7 +66,7 @@ const byte SCAN_DATA_ADDRESS = 0x01;     // Data address sent during Modbus RTU 
 const uint16_t SCAN_TIMEOUT = 200;       // Timeout (ms) for Modbus scan requests
 
 const byte MAC_START[3] = { 0x90, 0xA2, 0xDA };  // MAC range for Gheo SA
-#define ETH_RESET_PIN 7                          // Ethernet shield reset pin (deals with power on reset issue on low quality ethernet shields) \
+#define ETH_RESET_PIN 7                          // Ethernet shield reset pin (deals with power on reset issue on low quality ethernet shields)
                                                  // Comment out to disable the functionality
 const uint16_t CHECK_ETH_INTERVAL = 2000;        // Interval (ms) to check SPI connection with ethernet shield
 const uint16_t ETH_RESET_DELAY = 500;            // Delay (ms) during Ethernet start, wait for Ethernet shield to start (reset issue on low quality ethernet shields)
